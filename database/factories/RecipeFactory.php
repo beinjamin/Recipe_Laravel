33d13bc3e49e62fae964351f5a4465a85a7e69c5
beinjamin <jauresbeinjamin@gmail.com>
2022-04-17 11:33:32 +0100

    Creation des factory pour la generation fausse donnee

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Recipe>
  */
-class RecipeFactorFactory extends Factory
+class RecipeFactory extends Factory
 {
     /**
      * Define the model's default state.
